a6f036f4baeb542127caaacb90a3860f42db658b
Pawel Lesniowski <pawel_lesniowski@epam.com>
2018-02-20 13:59:30 +0100

    from parent to child

---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css']
+})
+export class ServerElementComponent implements OnInit {
+  @Input() element: {type: string, content: string, name: string};
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
